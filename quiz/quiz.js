e1e2a7bac22623fb55a28f16ed9777e8436401c8
Simon Pinkas <simon@code-think.com>
2022-03-31 19:54:54 +0200

    Implement scoring timer

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -4,14 +4,15 @@ import "../css/reset.css";
 import "../css/global.css";
 import "../css/quiz.css";
 
-let quiz;
+const QUESTION_TIME = 10000;
 
+let quiz;
 let startButton = document.getElementById("start-quiz");
 
 startButton.addEventListener("click", () => {
   //   On détermine la quantité de questions à afficher dans ces parenthèses.
   quiz = new QuizManager(5);
-  quiz.handleButtons()
+  quiz.handleInterface();
 });
 
 class QuizManager {
@@ -19,6 +20,8 @@ class QuizManager {
     this.numQuestions = numQuestions;
     this.questions = this.init();
     this.questionIndex = 0;
+    this.countdown = QUESTION_TIME;
+    this.timeRemaining = QUESTION_TIME;
     this.score = 0;
     this.timer = null;
     this.isRunning = true;
@@ -44,6 +47,7 @@ class QuizManager {
   }
 
   nextQuestion() {
+    this.timer.reset();
     this.questionIndex++;
     if (this.questionIndex >= this.questions.length) {
       this.endQuiz();
@@ -53,13 +57,25 @@ class QuizManager {
   }
 
   showQuestion() {
-      this.questionAnswered = false;
+    this.questionAnswered = false;
     const question = this.questions[this.questionIndex];
     const questionContainer = document.querySelector(
       "#question-container h1"
     );
-    questionContainer.innerHTML = question.question;
+    const scoreContainer = document.querySelector("#score");
+    questionContainer.innerHTML = `${question.question}`;
+
+    this.timer = new Timer();
     this.showAnswers();
+    this.timer.start();
+    let refreshTimer = setInterval(() => {
+      this.timeRemaining = this.countdown - this.timer.getTime();
+      scoreContainer.innerHTML = `${this.timeRemaining}`;
+      if (this.timeRemaining <= 0) {
+        this.nextQuestion();
+        clearInterval(refreshTimer);
+      }
+    }, 100);
   }
 
   showAnswers() {
@@ -83,12 +99,13 @@ class QuizManager {
   }
 
   validateAnswer(answer, el) {
+    this.timer.stop();
     if (
       this.questions[this.questionIndex].answers.find(
         (a) => a.text === answer
       ).correct
     ) {
-      this.score++;
+      this.score = this.score + 1 * this.timeRemaining;
       el.classList.add("correct");
     } else {
       el.classList.add("wrong");
@@ -96,15 +113,18 @@ class QuizManager {
     setTimeout(this.nextQuestion.bind(this), 1000);
   }
 
-  handleButtons() {
+  handleInterface() {
     const startButton = document.getElementById("start-quiz");
+    const scoreContainer = document.querySelector("#score");
 
     if (this.isRunning === true) {
-        startButton.style.display = "none";
-      } else {
-        startButton.style.display = "flex";
-        startButton.innerHTML = "Recommencer";
-      }
+      startButton.style.display = "none";
+      scoreContainer.style.display = "block";
+    } else {
+      scoreContainer.style.display = "none";
+      startButton.style.display = "flex";
+      startButton.innerHTML = "Recommencer";
+    }
   }
 
   endQuiz() {
@@ -115,9 +135,70 @@ class QuizManager {
     const questionContainer = document.querySelector(
       "#question-container h1"
     );
-
+    
+    this.timer = null;
     answerContainer.innerHTML = "";
     questionContainer.innerHTML = `Bravo, vous avez fait ${this.score} points`;
-    this.handleButtons()
+    this.handleInterface();
+  }
+}
+
+class Timer {
+  constructor() {
+    this.isRunning = false;
+    this.startTime = 0;
+    this.overallTime = 0;
+  }
+
+  _getTimeElapsedSinceLastStart() {
+    if (!this.startTime) {
+      return 0;
+    }
+
+    return Date.now() - this.startTime;
+  }
+
+  start() {
+    if (this.isRunning) {
+      return console.error("Timer is already running");
+    }
+
+    this.isRunning = true;
+
+    this.startTime = Date.now();
+  }
+
+  stop() {
+    if (!this.isRunning) {
+      return console.error("Timer is already stopped");
+    }
+
+    this.isRunning = false;
+
+    this.overallTime =
+      this.overallTime + this._getTimeElapsedSinceLastStart();
+  }
+
+  reset() {
+    this.overallTime = 0;
+
+    if (this.isRunning) {
+      this.startTime = Date.now();
+      return;
+    }
+
+    this.startTime = 0;
+  }
+
+  getTime() {
+    if (!this.startTime) {
+      return 0;
+    }
+
+    if (this.isRunning) {
+      return this.overallTime + this._getTimeElapsedSinceLastStart();
+    }
+
+    return this.overallTime;
   }
 }
