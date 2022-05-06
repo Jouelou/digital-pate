00b54563c80d12de8fda3a929722d0e8c2916846
Jouelou <joelbruchez99@gmail.com>
2022-05-06 15:33:33 +0200

    Test overflow fix

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 import "./css/reset.css";
 import "./css/global.css";
 import "./css/home.css";
-
-document.addEventListener('touchmove', function (event) {
-    if (event.scale !== 1) { event.preventDefault(); }
-  }, false);
