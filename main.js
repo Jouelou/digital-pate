0727ae0e6f974f7e1a13e73322d7efd9b4d223a3
Jouelou <joelbruchez99@gmail.com>
2022-05-06 15:12:10 +0200

    maintenant y'a même des traitillés !
    amélioration du css et ajout de traitillé dans la partie tuto.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import "./css/reset.css";
 import "./css/global.css";
 import "./css/home.css";
+
+document.addEventListener('touchmove', function (event) {
+    if (event.scale !== 1) { event.preventDefault(); }
+  }, false);
